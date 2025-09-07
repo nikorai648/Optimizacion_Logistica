8f1aa53978e293931beac6b57a722dd31b74be28
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-09-07 18:00:48 -0300

    commit

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Trabajador(models.Model):
+    # Identificación
+    rut = models.CharField(max_length=12, unique=True)
+    nombre = models.CharField(max_length=60)
+    apellido = models.CharField(max_length=60)
+    fecha_nacimiento = models.DateField()
+    email = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=20, blank=True)
