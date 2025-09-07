5bc0328264fd9d466925f2875ccadf3651614374
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-09-07 01:56:05 -0300

    Initial commit

---
+++ Optimizacion_Logistica/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Optimizacion_Logistica project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Optimizacion_Logistica.settings')
+
+application = get_wsgi_application()
---
+++ README.md
@@ -0,0 +1,2 @@
+# Optimizacion_Logistica
+ 
