ecd78d382975100caedacb590fcfc8facced70f2
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-09-07 21:09:07 -0300

    commit

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Trabajador, Asistencia, Accidente, TrabajadorAccidente
+from .models import Trabajador, Asistencia, Accidente
 
 @admin.register(Trabajador)
 class TrabajadorAdmin(admin.ModelAdmin):
@@ -7,14 +7,22 @@ class TrabajadorAdmin(admin.ModelAdmin):
     search_fields = ("rut", "nombre", "apellido", "email", "cargo")
     list_filter = ("estado", "turno", "tipo_contrato", "area")
 
+
 @admin.register(Asistencia)
 class AsistenciaAdmin(admin.ModelAdmin):
     list_display = ("trabajador", "fecha", "estado", "minutos_atraso", "horas_extras")
     list_filter = ("estado", "fecha")
     search_fields = ("trabajador__nombre", "trabajador__apellido", "trabajador__rut")
 
+
 @admin.register(Accidente)
 class AccidenteAdmin(admin.ModelAdmin):
-    list_display = ("fecha", "tipo", "gravedad", "lugar", "requiere_licencia", "dias_licencia")
+    list_display = ("fecha", "tipo", "gravedad", "lugar", "requiere_licencia", "dias_licencia", "involucrados")
     list_filter = ("gravedad", "requiere_licencia", "fecha")
-    search_fields = ("tipo", "lugar", "reportado_a")
+    search_fields = ("tipo", "lugar", "reportado_a", "trabajadores__rut", "trabajadores__nombre", "trabajadores__apellido")
+    filter_horizontal = ("trabajadores",)  # 👈 facilita seleccionar múltiples trabajadores
+
+    def involucrados(self, obj):
+        # Muestra cuántos trabajadores están asociados al accidente
+        return obj.trabajadores.count()
+    involucrados.short_description = "N° trabajadores"
