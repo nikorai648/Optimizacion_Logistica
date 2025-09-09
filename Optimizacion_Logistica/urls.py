29432310ed9ef3a69f3438101d5c84b9dd7a2b10
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-09-08 21:42:51 -0300

    commit

--- Optimizacion_Logistica/urls.py
+++ Optimizacion_Logistica/urls.py
@@ -20,6 +20,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/',  auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 
     path('', include('core.urls')),
