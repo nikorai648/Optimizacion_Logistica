2f190885fccb2cf36acd9506e0b3df7c04a56d59
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-10-07 08:42:00 -0300

    cambio

--- Optimizacion_Logistica/settings.py
+++ Optimizacion_Logistica/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
-import pymysql
+
 from pathlib import Path
 
-pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'Optimizacion_Logistica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DJANGO_DEMO_EMP',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
