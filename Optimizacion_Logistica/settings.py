e1ffe20e8e8719ae300b264a26d51c46af9e7bfe
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-10-18 18:58:30 -0300

    commit

--- Optimizacion_Logistica/settings.py
+++ Optimizacion_Logistica/settings.py
@@ -77,9 +77,12 @@ WSGI_APPLICATION = 'Optimizacion_Logistica.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = { 'default': 
-             { 'ENGINE': 'django.db.backends.sqlite3', 
-              'NAME': BASE_DIR / 'db.sqlite3', } }
+DATABASES ={
+     'default': 
+     { 'ENGINE': 'django.db.backends.sqlite3', 
+      'NAME': BASE_DIR / 'db.sqlite3',
+        } 
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
