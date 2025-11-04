ac8caae38b7a03a10dadd2215605aa09e41e663a
nikorai648 <169163960+nikorai648@users.noreply.github.com>
2025-11-04 09:00:52 -0300

    commit

--- Optimizacion_Logistica/settings.py
+++ Optimizacion_Logistica/settings.py
@@ -54,7 +54,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'optimizacion_logistica',
-        'USER': 'admin',
+        'USER': 'django_user',
         'PASSWORD': '6487063a1234',    
         'HOST': '127.0.0.1',
         'PORT': '3306',
